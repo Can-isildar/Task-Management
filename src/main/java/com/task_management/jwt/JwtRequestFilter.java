fc54837d497ea539e9ab47bdff65a49122103c1e
can <82100395+Can-isildar@users.noreply.github.com>
2024-10-02 11:05:00 +0300

    basic auth ve jwt Bearer token auth eklendi

--- src/main/java/com/task_management/jwt/JwtRequestFilter.java
+++ src/main/java/com/task_management/jwt/JwtRequestFilter.java
@@ -5,27 +5,34 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
-import com.task_management.jwt.JwtTokenUtil;
 import java.io.IOException;
 
 @Service
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-    @Autowired
-    UserService userService;
+
+    private final UserService userService;
+
+    private final JwtTokenUtil jwtTokenUtil;
 
     @Autowired
-    JwtTokenUtil jwtTokenUtil;
+    @Lazy
+    public JwtRequestFilter(UserService userService, JwtTokenUtil jwtTokenUtil) {
+        this.userService = userService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
 
         String jwt = null;
@@ -33,9 +40,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
-            username = jwt.substring(7);
+            username = jwtTokenUtil.extractUsername(jwt);
         }
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userService.loadUserByUsername(username);
             if (jwtTokenUtil.validateToken(jwt, userDetails)) {
 
--- src/main/java/com/task_management/security/SecurityConfig.java
+++ src/main/java/com/task_management/security/SecurityConfig.java
@@ -1,22 +1,28 @@
 package com.task_management.security;
 
+import com.task_management.jwt.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter; // JWT filtresini ekliyoruz
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,7 +37,6 @@ public class SecurityConfig {
         return new ProviderManager(daoAuthenticationProvider);
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf(csrf -> csrf.disable())
@@ -40,8 +45,10 @@ public class SecurityConfig {
                     auth.requestMatchers("/auth/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT için stateless oturum
                 .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class) // JWT filtresini ekliyoruz
                 .build();
     }
 }
-
